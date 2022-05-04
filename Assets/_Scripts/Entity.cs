098cb66820f951e5702a86551158b3b40feefb46
Nick Oostdyk <44105028+xxstdyk@users.noreply.github.com>
2022-05-03 22:34:14 -0400

    Initial Commit
    thar be islands ahoy arrr

---
+++ Assets/_Scripts/Entity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+	private Rigidbody2D _rigidbody;
+	private float _maxSpeed = 10f;
+	private float _acceleration = 100f;
+
+	private void Awake() => _rigidbody = GetComponent<Rigidbody2D>();
+
+	public void Move(Vector2 direction)
+	{
+		direction.Normalize();
+
+		// apply delta velocity and clamp to max speed
+		_rigidbody.velocity += direction * _acceleration * Time.deltaTime;
+		_rigidbody.velocity = Vector2.ClampMagnitude(_rigidbody.velocity, _maxSpeed);
+
+		// apply damping force if input is 0 or velocity is opposed
+		var d = Vector2.Dot(_rigidbody.velocity, direction);
+		if (direction == Vector2.zero || d < 0.1f)
+			_rigidbody.velocity = Vector2.Lerp(_rigidbody.velocity, Vector2.zero, .15f);
+	}
+}
---
+++ Assets/_Scripts/PlayerInputHandler.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+	[Header("Input Axis")]
+	[SerializeField] private InputAction _axisAction;
+	[Header("Interact")]
+	[SerializeField] private InputAction _interactAction;
+
+	[System.Flags]
+	private enum InputState : short
+	{
+		NONE = 0b0,
+		INTERACT = 0b1,
+	}
+
+	private Entity _player;
+	private InputState _inputState;
+	private Vector2 _movementInput;
+
+	private void Start()
+	{
+		_player = GetComponent<Entity>();
+
+		_inputState = 0;
+		_movementInput = Vector2.zero;
+	}
+
+	private void Update()
+	{
+		if (_interactAction.WasPerformedThisFrame())
+			_inputState |= InputState.INTERACT;
+
+		_movementInput = _axisAction.ReadValue<Vector2>();
+	}
+
+	private void FixedUpdate()
+	{
+		_player.Move(_movementInput);
+
+		_inputState = 0;
+		_movementInput = Vector2.zero;
+	}
+
+	private void OnEnable()
+	{
+		_axisAction.Enable();
+		_interactAction.Enable();
+	}
+
+	private void OnDisable()
+	{
+		_axisAction.Disable();
+		_interactAction.Disable();
+	}
+
+}
